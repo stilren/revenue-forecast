7a8305636e94e4c0738b0289be4de0f1257f1eba
sebastian <sebastian.wassberg@digitalent.se>
2019-08-21 22:23:30 +0200

    Updates

---
+++ precache-manifest.40ec6e2db9fc03bc2c2ea05d3fbb7daf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1de5a2902b0be91cb1f8ccb1e284b991",
+    "url": "/revenue-forecast/index.html"
+  },
+  {
+    "revision": "e8a76488f35bb14391d2",
+    "url": "/revenue-forecast/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "fa540060d6e432242358",
+    "url": "/revenue-forecast/static/js/2.de0836ec.chunk.js"
+  },
+  {
+    "revision": "e8a76488f35bb14391d2",
+    "url": "/revenue-forecast/static/js/main.ce2d42d3.chunk.js"
+  },
+  {
+    "revision": "4f28568f2cd5188775cf",
+    "url": "/revenue-forecast/static/js/runtime~main.ce5ea411.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/revenue-forecast/static/media/logo.5d5d9eef.svg"
+  }
+]);
